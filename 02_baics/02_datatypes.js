91b421c9277b17b199b01ad924dd38750d9e46a7
Yash Pawar <113225137+HackersParadisee@users.noreply.github.com>
2025-06-28 16:44:20 +0000

    datatypes

---
+++ 02_baics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // this says that it will treat all code as newer version
+// alert(3+3) // we are using nodejs not browser 
+
+// code redablity should be high 
+// ECMA standards is documentation for javascript 
+// MDN is also one easy document 
+
+console.log("yash");
+
+// data types 
+
+let name = "yash" // string
+let age = 22 // number 
+let isLogged = false //boolean
+
+
+// premetive data types :
+// number 
+// bigint   // large numbers 
+// string   // can be written in double qoutes "" and also in single qoutes ''
+// boolean  // true or false 
+// null     // stand alone value eg ( let temp=null;)
+// undefined // when value of variable is not defined for eg : (let name;)
+// symbol   // unique 
+
+console.log(typeof "yash is my name") // it will say string after execution 
+console.log(typeof age)
+console.log(typeof null) // it will say object 
+
+
+
+
